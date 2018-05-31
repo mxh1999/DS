9265981314db39ea48f3af806a1f6298d61f0702
mxh1999 <30319236+mxh1999@users.noreply.github.com>
2018-05-31 21:18:05 +0800

    change

--- application/models/ticket_model.php
+++ application/models/ticket_model.php
@@ -94,13 +94,11 @@ class Calculate_model extends CI_Model {
 		while ($out = socket_read($socket,8192));
 		return intval($out);
 	}
-	
 	/**
-	 * 查询用户信息
-	 * 返回 -1 = 服务器炸了
-	 * 返回 (name,email,phone,privilege)
+	 * 退订车票
+	 * -1 = 服务器炸了 0 = 失败 1 = 成功
 	 */
-	public function query($id)
+	public function refund($id, $num, $train_id, $loc1, $loc2, $date, $ticket_kind)
 	{
 		$socket = socket_create(AF_INET, SOCK_STREAM, SOL_TCP);
 		if ($socket < 0)	return -1;
@@ -110,7 +108,7 @@ class Calculate_model extends CI_Model {
 			socket_close($socket);
 			return -1;
 		}
-		$in = "query_profile " . $id . "\n";
+		$in = "buy_ticket " . $id . " " . $num . " " . $train_id . " " . $loc1 . " " . $loc2 . " " .$date. " " . ticket_kind . "\n";
 		if (!socket_write($socket ,$in, strlen($in)))
 		{
 			socket_close($socket);
@@ -118,63 +116,6 @@ class Calculate_model extends CI_Model {
 		}
 		$out='';
 		while ($out = socket_read($socket,8192));
-		$tmp = explode(" ",$out);
-		$ans = array("name" => tmp[0], "email" => tmp[1], "phone" => intval(tmp[2]), "privilege" => inval(tmp[3]));
-		return ans;
-	}
-	/**
-	 * 修改用户信息
-	 * -1 = 服务器炸了
-	 * 0 = 密码错误
-	 * 1 = 成功
-	 */
-	public function modify_profile($id, $name, $psword, $email, $phone)
-	{
-		$socket = socket_create(AF_INET, SOCK_STREAM, SOL_TCP);
-		if ($socket < 0)	return -1;
-		$result = socket_connect($socket, $DATABASE_IP, $DATABASE_PORT);
-		if ($result < 0)
-		{
-			socket_close($socket);
-			return -1;
-		}
-		$psword_md5 = md5($psword);
-		$in = "modify_profile ". $id . " " . $name . " " . $psword_md5 . " " . $email ." " . $phone . "\n";
-		if (!socket_write($socket ,$in, strlen($in)))
-		{
-			socket_close($socket);
-			return -1;
-		}
-		$out='';
-		while ($out = socket_read($socket,8192));
-		socket_close($socket);
-		return intval($out);
-	}
-	/**
-	 * 修改用户权限
-	 * -1 = 服务器炸了
-	 * 0 = 失败
-	 * 1 = 成功
-	 */
-	public function modify_privilege($id1, $id2, $privilege)
-	{
-		$socket = socket_create(AF_INET, SOCK_STREAM, SOL_TCP);
-		if ($socket < 0)	return -1;
-		$result = socket_connect($socket, $DATABASE_IP, $DATABASE_PORT);
-		if ($result < 0)
-		{
-			socket_close($socket);
-			return -1;
-		}
-		$in = "modify_privilege " . $id1 . " " . $id2 . " " . $privilege . "\n";
-		if (!socket_write($socket ,$in, strlen($in)))
-		{
-			socket_close($socket);
-			return -1;
-		}
-		$out='';
-		while ($out = socket_read($socket,8192));
-		socket_close($socket);
 		return intval($out);
 	}
 }
