6e8d7d24e58df14a082581df59d50ca3b8687c17
mxh1999 <30319236+mxh1999@users.noreply.github.com>
2018-06-13 01:44:57 +0800

    update

--- application/models/Train_model.php
+++ application/models/Train_model.php
@@ -9,6 +9,40 @@ class Ticket_model extends CI_Model {
 	}
 	function public add_train($train_id,$name,$catalog,$num_station,$num_price,$Price,$Station)
 	{
+		$socket = socket_create(AF_INET, SOCK_STREAM, SOL_TCP);
+		if ($socket < 0)	return -1;
+		$result = socket_connect($socket, $DATABASE_IP, $DATABASE_PORT);
+		if ($result < 0)
+		{
+			socket_close($socket);
+			return -1;
+		}
+		$in = "add_train " . $train_id . " " . $name . ' ' . $catalog . ' ' . $num_station;
+		for ($i = 0; $i < $num_station;$i++)
+		{
+			$in .= ' ' . $Price['name'][$i];
+		}
+		$in .= '#';
+		if (!socket_write($socket ,$in, strlen($in)))
+		{
+			socket_close($socket);
+			return -1;
+		}
+		for ($i = 0;$i < $num_station;$i++)
+		{
+			$in = Station['name'][$i] . ' ' . Station['arr'][$i] . ' ' . Station['sta'][$i] . ' ' . Station['sto'][$i];
+			for ($j = 0; $j< $num_price;$j++)
+				$in .= ' ' . $Price['num'][$i][$j];
+			$in .= '#';
+			if (!socket_write($socket ,$in, strlen($in)))
+			{
+				socket_close($socket);
+				return -1;
+			}
+		}
+		$out = socket_read($socket,8192);
+		while (!isset($out))	$out = socket_read($socket,8192);
+		return intval($out);
 	}
 	function public modify_train($train_id,$name,$catalog,$num_station,$num_price,$Price,$Station)
 	{
@@ -32,8 +66,8 @@ class Ticket_model extends CI_Model {
 			socket_close($socket);
 			return -1;
 		}
-		$out = '';
-		while ($out = socket_read($socket,8192));
+		$out = socket_read($socket,8192);
+		while (!isset($out))	$out = socket_read($socket,8192);
 		return intval($out);
 	}
 	function public delete_train($train_id)
@@ -52,8 +86,8 @@ class Ticket_model extends CI_Model {
 			socket_close($socket);
 			return -1;
 		}
-		$out = '';
-		while ($out = socket_read($socket,8192));
+		$out = socket_read($socket,8192);
+		while (!isset($out))	$out = socket_read($socket,8192);
 		return intval($out);
 	}
 }
