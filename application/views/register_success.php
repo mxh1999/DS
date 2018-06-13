13b756f58d37c4c39a4e1cf4360c713a80d2303e
wgy0625 <39829509+wgy0625@users.noreply.github.com>
2018-06-13 16:36:51 +0800

    change

--- application/views/register_success.php
+++ application/views/register_success.php
@@ -5,7 +5,7 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 <html>
 <head>
    <meta charset="utf-8"> 
-   <title>RE</title>
+   <title>注册成功</title>
    <link rel="stylesheet" href="https://cdn.bootcss.com/bootstrap/3.3.7/css/bootstrap.min.css">  
    <script src="https://cdn.bootcss.com/jquery/2.1.1/jquery.min.js"></script>
    <script src="https://cdn.bootcss.com/bootstrap/3.3.7/js/bootstrap.min.js"></script>
