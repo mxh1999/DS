eb17fc85b6743a335329e107b11f290e8c328b57
mxh1999 <30319236+mxh1999@users.noreply.github.com>
2018-06-15 00:53:36 +0800

    try

--- application/controllers/Ticket.php
+++ application/controllers/Ticket.php
@@ -37,6 +37,8 @@ class Ticket extends CI_Controller {
 			$loc2=$this->input->post('loc2');
 			$date=$this->input->post('date');
 			$ticket_kind=$this->input->post('ticket_kind');
+			var_dump($ticket_num);
+			die();
 			$ok=$this->Ticket_model->buy($id,$train_id,$loc1,$loc2,$date,$ticket_kind,$ticket_num);
 			if ($ok === -1)
 			{
