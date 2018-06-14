322f14633f65cc9d22d5ca2a8548461f48e286b6
mxh1999 <30319236+mxh1999@users.noreply.github.com>
2018-06-15 04:16:19 +0800

    add

---
+++ application/views/goto.php
@@ -0,0 +1,9 @@
+<?php
+defined('BASEPATH') OR exit('No direct script access allowed');
+?>
+<!DOCTYPE html>
+<html>
+	<head>
+		<meta http-equiv="refresh" content="0; url = <?php echo $url; ?>">
+	</head>
+</html>
